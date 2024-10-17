1999d5191a0ae20b056f71b5bde55e00b12b07ae
Bejan Andreea <andreeabejan03@gmail.com>
2024-10-17 15:16:11 +0300

    Translated column names in graphical illustrations

--- graphical_illustration.py
+++ graphical_illustration.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from deep_translator import GoogleTranslator
 
 data = pd.read_excel('Data cat personality and predation Cordonnier et al.xlsx')
 
+
 for col in data.columns:
-    if col != 'Race':
+    if col != 'Race' and col != 'Row.names' and col != 'Plus':
+
         if data[col].dtype == 'object' or data[col].dtype == 'category':
             plt.figure(figsize=(10, 6))
             sns.countplot(data=data, x='Race', hue=col)
-            plt.title(f'Countplot: Race vs {col}')
+            plt.title(f'Countplot: Race vs {GoogleTranslator(source='auto', target='en').translate(col)}')
             plt.xticks(rotation=45)
             plt.tight_layout()
             plt.show()
@@ -17,7 +20,7 @@ for col in data.columns:
         elif pd.api.types.is_numeric_dtype(data[col]):
             plt.figure(figsize=(10, 6))
             sns.boxplot(data=data, x='Race', y=col)
-            plt.title(f'Boxplot: Race vs {col}')
+            plt.title(f'Boxplot: Race vs {GoogleTranslator(source='auto', target='en').translate(col)}')
             plt.xticks(rotation=45)
             plt.tight_layout()
             plt.show()
