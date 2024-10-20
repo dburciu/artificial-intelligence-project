63040b183e6af28ccc18f7e3d2e388421b96cf24
rozalinutza22 <rozalinutza@protonmail.com>
2024-10-20 11:09:56 +0300

    Modified unique_values as a module

--- unique_values_list.py
+++ unique_values_list.py
@@ -1,22 +1,24 @@
 import pandas as pd
 
-data = pd.read_excel('Data cat personality and predation Cordonnier et al.xlsx')
+def unique_val():
 
-# Get a list of all columns to identify class columns
-attributes = data.columns.tolist()
+    data = pd.read_excel('Data cat personality and predation Cordonnier et al.xlsx')
 
-# Non-numeric columns
-classes = data.select_dtypes(include=['object']).columns.tolist()
+    # Get a list of all columns to identify class columns
+    attributes = data.columns.tolist()
 
-for column in attributes:
-    print(f"\nProcessing column: {column}")
+    # Non-numeric columns
+    classes = data.select_dtypes(include=['object']).columns.tolist()
+
+    for column in attributes:
+        print(f"\nProcessing column: {column}")
     
-    unique_values = data[column].value_counts()
+        unique_values = data[column].value_counts()
     
-    print(f"Total unique values in {column}: {len(unique_values)}")
-    print(unique_values)
+        print(f"Total unique values in {column}: {len(unique_values)}")
+        print(unique_values)
 
-    for class_column in classes:
-        if class_column != column: 
-            grouped = data.groupby(class_column)[column].value_counts()
-            print(grouped)
+        for class_column in classes:
+            if class_column != column: 
+                grouped = data.groupby(class_column)[column].value_counts()
+                print(grouped)
